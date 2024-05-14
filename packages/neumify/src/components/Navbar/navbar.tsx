ae6a315bb37fa9ff1520e57c587881f2faff5495
Zhu <zhuyuezhan@foxmail.com>
2024-05-14 15:24:10 +0800

    feat: change Navbar to uncontrolled, add hover effects

--- packages/neumify/src/components/Navbar/navbar.tsx
+++ packages/neumify/src/components/Navbar/navbar.tsx
@@ -11,24 +11,22 @@ interface NavbarProps
   extends Omit<React.HTMLAttributes<HTMLUListElement>, 'onChange'> {
   items: ItemVo[]
   onChange: (item: ItemVo) => void
-  defaultActiveKey: string | number
+  activeKey?: string | number
   className?: string
 }
 const Navbar = React.forwardRef<HTMLUListElement, NavbarProps>(
-  ({ items, defaultActiveKey, onChange, className, ...props }, ref) => {
-    const [activeKey, setActiveKey] = useState<any>(defaultActiveKey)
+  ({ items, activeKey, onChange, className, ...props }, ref) => {
     const sliderId = useId()
 
     const handleLinkClick = (item: ItemVo) => {
       if (item.key !== activeKey) {
         onChange(item)
-        setActiveKey(item.key)
       }
     }
     return (
       <ul
         className={cn(
-          'bg-base border-dark shadow-outset-3 relative flex w-fit h-fit min-h-12 list-none rounded border px-4',
+          'bg-base border-dark shadow-outset-3 relative flex h-fit min-h-12 w-fit list-none rounded border px-4',
           className
         )}
         ref={ref}
@@ -38,7 +36,9 @@ const Navbar = React.forwardRef<HTMLUListElement, NavbarProps>(
           return (
             <li
               key={item.key}
-              className={cn('relative flex items-center justify-center ')}
+              className={cn(
+                'hover:shadow-inset-2 relative flex cursor-pointer items-center justify-center'
+              )}
               onClick={() => handleLinkClick(item)}
             >
               {item.key === activeKey && (
@@ -48,7 +48,7 @@ const Navbar = React.forwardRef<HTMLUListElement, NavbarProps>(
                     'z-2 shadow-inset-2 border-dark absolute h-full w-full border-x'
                   )}
                   transition={{ duration: 0.25 }}
-                ></motion.div>
+                />
               )}
               {item.children}
             </li>
