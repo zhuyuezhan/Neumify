b73ecef782ef7a81b612be91127ff0a2a64e8c74
Zhu <zhuyuezhan@foxmail.com>
2024-05-15 15:04:38 +0800

    feat: some style changes

--- packages/neumify/src/components/Accordion/accordion.tsx
+++ packages/neumify/src/components/Accordion/accordion.tsx
@@ -51,13 +51,13 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        'text-primary flex flex-1 items-center justify-between p-4 font-medium transition-all [&[data-state=open]>svg]:rotate-45',
+        'flex flex-1 items-center justify-between p-4 font-medium transition-all [&[data-state=open]>svg]:rotate-45',
         className
       )}
       {...props}
     >
       {children}
-      <Plus className="h-4 w-4 shrink-0 transition-transform duration-200 text-black font-extrabold" />
+      <Plus className="h-4 w-4 shrink-0 font-extrabold text-black transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
@@ -69,7 +69,7 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down text-secondary overflow-hidden text-sm transition-all"
+    className="text-secondary overflow-hidden text-sm transition-all data-[state=closed]:h-0 "
     {...props}
   >
     <div className={cn('px-4 pb-2', className)}>{children}</div>
