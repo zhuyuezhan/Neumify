a38cca13cfc51e67d613b3cdeb383dbbecd199ca
Zhu <zhuyuezhan@foxmail.com>
2024-05-16 03:52:02 +0800

    feat: update Slider to support vertical mode, support range selector, and temporary remove controlled mode prop value

--- packages/neumify/src/components/Slider/slider.tsx
+++ packages/neumify/src/components/Slider/slider.tsx
@@ -3,9 +3,16 @@ import * as SliderPrimitive from '@radix-ui/react-slider'
 import { cn } from '@/common/utils'
 
 interface SliderProps
-  extends React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> {
+  extends Omit<
+    React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>,
+    'value'
+  > {
   color?: string
 }
+
+const Thumb = () => (
+  <SliderPrimitive.Thumb className="border-dark shadow-outset-3 bg-base ring-offset-background block aspect-square w-5 rounded-full border transition-all hover:w-6 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50" />
+)
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
@@ -15,18 +22,19 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Root
       ref={ref}
       className={cn(
-        'relative flex w-full touch-none select-none items-center',
+        'relative flex touch-none select-none items-center data-[orientation=vertical]:flex-col',
         className
       )}
       {...props}
     >
-      <SliderPrimitive.Track className="shadow-inset-2 border-dark bg-base relative h-2 w-full grow overflow-hidden rounded-full border">
+      <SliderPrimitive.Track className="shadow-inset-2 border-dark bg-base relative grow overflow-hidden rounded-full border data-[orientation=horizontal]:h-2 data-[orientation=vertical]:h-40 data-[orientation=horizontal]:w-40 data-[orientation=vertical]:w-2">
         <SliderPrimitive.Range
-          className="bg-primary absolute h-full"
+          className="bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full"
           style={{ backgroundColor: color }}
         />
       </SliderPrimitive.Track>
-      <SliderPrimitive.Thumb className="border-dark shadow-outset-3 bg-base ring-offset-background block aspect-square w-5 rounded-full border transition-all hover:w-6 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50" />
+      <Thumb />
+      {props.defaultValue && props.defaultValue[1] && <Thumb />}
     </SliderPrimitive.Root>
   )
 })
