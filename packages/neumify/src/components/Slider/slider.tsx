00209877ac6fd7d9f92888b038395d3f4fd8f39c
Zhu <zhuyuezhan@foxmail.com>
2024-05-15 21:33:48 +0800

    feat: update Slider

--- packages/neumify/src/components/Slider/slider.tsx
+++ packages/neumify/src/components/Slider/slider.tsx
@@ -2,24 +2,34 @@ import React from 'react'
 import * as SliderPrimitive from '@radix-ui/react-slider'
 import { cn } from '@/common/utils'
 
+interface SliderProps
+  extends React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> {
+  color?: string
+}
+
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
-    className={cn(
-      'relative flex w-full touch-none select-none items-center',
-      className
-    )}
-    {...props}
-  >
-    <SliderPrimitive.Track className="shadow-inset-default bg-secondary relative h-2 w-full grow overflow-hidden rounded-full">
-      <SliderPrimitive.Range className="bg-primary absolute h-full" />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="shadow-outset-default hover:shadow-outset-activate sd-transition bg-base ring-offset-background block h-5 w-5 rounded-full transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50" />
-  </SliderPrimitive.Root>
-))
+  SliderProps
+>(({ color, className, ...props }, ref) => {
+  return (
+    <SliderPrimitive.Root
+      ref={ref}
+      className={cn(
+        'relative flex w-full touch-none select-none items-center',
+        className
+      )}
+      {...props}
+    >
+      <SliderPrimitive.Track className="shadow-inset-2 border-dark bg-base relative h-2 w-full grow overflow-hidden rounded-full border">
+        <SliderPrimitive.Range
+          className="bg-primary absolute h-full"
+          style={{ backgroundColor: color }}
+        />
+      </SliderPrimitive.Track>
+      <SliderPrimitive.Thumb className="border-dark shadow-outset-3 bg-base ring-offset-background block aspect-square w-5 rounded-full border transition-all hover:w-6 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50" />
+    </SliderPrimitive.Root>
+  )
+})
 Slider.displayName = 'Slider'
 
 export { Slider }
